ee7ffdfe59bbdac238f99ecf7556b6260f0831bf
Vladyslav Boichenko <vladyslav.boichenko@482.solutions>
2019-10-17 11:44:51 +0300

    Start

---
+++ studentsAndNumbers/StudentsAndNumbers/StudentsAndNumbers/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StudentsAndNumbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
