fe4510bdde11407ca0e1defa44940484f0427d10
Vladyslav Boichenko <vladyslav.boichenko@482.solutions>
2019-10-17 12:40:31 +0300

    stCG

---
+++ StrangeGame/StrangeGame/PlayingField.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StrangeGame
+{
+    class PlayingField
+    {
+        private int SizeX;
+        private int SizeY;
+        PlayingField( int _SizeX, int _SizeY)
+        {
+            SizeX = _SizeX;
+            SizeY = _SizeY;
+        }
+    }
+}
